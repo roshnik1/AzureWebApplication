b3f4675a3c2071f1a01879c67da4b9b49726ab68
Haneesha Dushara <haneesha207@gmail.com>
2024-04-21 23:28:21 -0400

    Fix database configuration and form field types in authentication module

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -2,18 +2,22 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+from flask_wtf import Form
+
+from wtforms import StringField, PasswordField
+# from wtforms.validators import Required
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import SubmitField
-from wtforms import TextField, PasswordField
+# from wtforms import StringField, 
 from wtforms.validators import Email, DataRequired
 
 # login and registration
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username',
+    username = StringField('Username',
                          id='username_login',
                          validators=[DataRequired()])
     password = PasswordField('Password',
@@ -22,10 +26,10 @@ class LoginForm(FlaskForm):
 
 
 class CreateAccountForm(FlaskForm):
-    username = TextField('Username',
+    username = StringField('Username',
                          id='username_create',
                          validators=[DataRequired()])
-    email = TextField('Email',
+    email = StringField('Email',
                       id='email_create',
                       validators=[DataRequired(), Email()])
     password = PasswordField('Password',
@@ -35,7 +39,7 @@ class CreateAccountForm(FlaskForm):
 class UploadForm(FlaskForm):
     file = FileField('File'),
     submit = SubmitField('Submit')
-    # username = TextField('Username',
+    # username = StringField('Username',
     #                      id='username_login',
     #                      validators=[DataRequired()])
  
